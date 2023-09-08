4eca627eed9f8cf7f8906e4879bb1ea65314fafa
montybasquiart <owmathias@gmail.com>
2023-09-08 23:14:11 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,27 +7,21 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 
-/* defining variables */
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
-	char charType;
-	int intType;
-	long int longType;
-	long long int longlongType;
-	float floatType;
-
-/**
- * sizeof: evaluates the size of a variable
- * printf: printsthe size of char, int, double, long int, long long int, and float
- * %zu: format for (z)length modifier and (u)unsigned type
- */
-	printf("Size of a char: %zu bytes\n", sizeof(charType));
-	printf("Size of an int: %zu bytes\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes\n", sizeof(longType));
-	printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
-	printf("Size of a float: %zu bytes\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %zu bytes\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %zu bytes\n", (unsigned long)sizeof(c));
+	printf("Size of long long int: %zu bytes\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %zu bytes\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
