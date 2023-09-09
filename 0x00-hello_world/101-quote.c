1df9d7b808e8a02326e6d49b322eacab2ca21dd4
montybasquiart <owmathias@gmail.com>
2023-09-09 00:16:56 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("and the piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
