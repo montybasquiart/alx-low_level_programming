7c0073dcf4e8b923c9bd4ebd3002ea9cf42a0efd
montybasquiart <owmathias@gmail.com>
2023-09-09 00:21:19 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 
 {
-    printf("and the piece of art is usefuli\" - Dora Korpar, 2015-10-19\n");
+printf("and the piece of art is usefuli\" - Dora Korpar, 2015-10-19\n");
 
-    return (1);
+return (1);
 }
