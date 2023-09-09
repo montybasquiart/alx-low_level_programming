ead81bdbe832adc99f5baf01d49d6a09baf02f55
montybasquiart <owmathias@gmail.com>
2023-09-09 00:22:29 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 
 {
-printf("and the piece of art is usefuli\" - Dora Korpar, 2015-10-19\n");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 }
