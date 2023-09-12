9a0828d7c04d012193eaeb5400d10fd5385078c9
montybasquiart <owmathias@gmail.com>
2023-09-12 02:58:19 +0000

    ./5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/* Declare variable and initialize range */
+
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+
+	/* print the value of the variable */
+
+		printf("%d", n);
+	}
+
+	printf("\n");
+
+	return (0);
+}
