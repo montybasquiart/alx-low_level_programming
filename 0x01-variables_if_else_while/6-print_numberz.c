e7fb8c96e3091b3a6eb3658f74baa115054399b1
montybasquiart <owmathias@gmail.com>
2023-09-12 03:05:49 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always o (Success)
+*/
+
+int main(void)
+{
+	/* Declare a variable and initialize */
+
+	int n;
+
+	for (n = 10; n < 10; n++)
+	{
+
+	/* Print the value of n */
+
+		putchar((n % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
