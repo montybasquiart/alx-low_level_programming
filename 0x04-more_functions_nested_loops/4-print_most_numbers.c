536ac4f41faebb3b5600e3f8fac65ce3b452ed22
montybasquiart <owamathias@gmail.com>
2023-11-17 20:10:57 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9
+ * except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		if(numb != 2 && numb != 4)
+		{
+			_putchar(numb + '0');
+		}
+	}
+	_putchar('\n');
+}
