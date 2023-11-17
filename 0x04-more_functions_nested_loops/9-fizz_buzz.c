4203484f0c177b98d8580ec4664b135c25edf010
montybasquiart <owamathias@gmail.com>
2023-11-17 21:30:15 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0 && (i % 5 == 0))
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 		printf("FizzBuzz ");
 		}
@@ -27,8 +27,9 @@ int main(void)
 		{
 		printf("%d ", i);
 		}
-		printf("\n");
-		return (0);
 	}
+	printf("Buzz");
+	printf("\n");
 
+	return (0);
 }
