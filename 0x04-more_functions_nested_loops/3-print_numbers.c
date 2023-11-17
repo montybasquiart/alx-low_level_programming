80045f3e750166add0bdde5b3c97217412c25cb9
montybasquiart <owamathias@gmail.com>
2023-11-17 19:53:30 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers range 0 - 9
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int pt;
+
+	for (pt = 48; pt <= 57; pt++)
+	{
+		_putchar(pt);
+	}
+	_putchar('\n');
+}
