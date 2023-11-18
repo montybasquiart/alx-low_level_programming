77e58a10a8d3231aa106f455906511702611f56a
montybasquiart <owamathias@gmail.com>
2023-11-18 18:50:52 +0000

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: 1st parameter, argc (argument count), no of command line arguments
+ * @argv: 2nd parameter, argv (argument vector), array that contains the arg
+ * Return: (0) when successful
+ */
+
+int main(int argc, char **argv)
+{
+	int cents, mncoin = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		if (cents >= 10)
+			cents -= 10;
+		if (cents >= 2)
+			cents -= 2;
+		if (cents >= 1)
+			cents -= 1;
+		mncoin += 1;
+	}
+	printf("%d", mncoin);
+	return (0);
+}
