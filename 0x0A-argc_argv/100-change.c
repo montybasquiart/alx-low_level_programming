567c354b0d655019ac3337698e5deda913a33fa4
montybasquiart <owamathias@gmail.com>
2023-11-18 19:02:58 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 			cents -= 10;
 		else if (cents >= 5)
 			cents -= 5;
-		else if(cents >= 2)
+		else if (cents >= 2)
 			cents -= 2;
 		else if (cents >= 1)
 			cents -= 1;
