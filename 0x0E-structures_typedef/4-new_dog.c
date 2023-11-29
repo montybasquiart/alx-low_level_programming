4e523c13801878a3c0b7e2ff3902abf30f44fec4
montybasquiart <owamathias@gmail.com>
2023-11-29 11:25:03 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-	{
 		return (NULL);
-	}
 	for (n1 = 0; name[n1]; n1++)
 		;
 	n1++;
