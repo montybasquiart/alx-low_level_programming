1877eea789016a608bebfd8100ec8dcd60bc359b
montybasquiart <owamathias@gmail.com>
2023-11-29 02:34:40 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a struct that creates dog data
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: defining dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
