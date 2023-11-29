3f150b36b6fe9afc1d30d7e37a321f5ae233aa20
montybasquiart <owamathias@gmail.com>
2023-11-29 01:17:05 +0000

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,7 +1,13 @@
-#ifndef MACROLIKE_H
-#define MACROLIKE_H
+#ifndef SUMXY_H
+#define SUMXY_H
 
 /**
- * 
- *
+ * File: 4-sum.h
+ * @x: first input
+ * @y: second input
+ * Desc: sum x and y
  */
+
+#define SUM(x, y) (x +y)
+
+#endif
