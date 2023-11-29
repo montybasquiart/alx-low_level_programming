08207f7d05794f636a5e2203695b1e2a8f087c87
montybasquiart <owamathias@gmail.com>
2023-11-29 01:05:56 +0000

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ * File: 3-function_like_macro
+ * @x: absolute value of a number
+ * Desc: header file that computes absolute value
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
