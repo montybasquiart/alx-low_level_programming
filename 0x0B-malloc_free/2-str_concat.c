7b9aa037ab91427e9a2b0be6cef2272b3cd6a560
montybasquiart <owamathias@gmail.com>
2023-12-12 20:05:17 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,21 +12,21 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int a, b, c, d;
 
-	if (s1 == NULL)
+	if (s1 == 0)
 		s1 = "";
-	if (s2 == NULL)
+	if (s2 == 0)
 		s2 = "";
 
 	for (a = 0; s1[a] != '\0'; a++)
 		;
 	for (b = 0; s2[b] != '\0'; b++)
 		;
-
-	s = malloc((a * sizeof(s1)) + (b * sizeof(*s2)) + 1);
-	if (s == NULL)
+	b++;
+	s = malloc(a * sizeof(s1) + (b * sizeof(*s2)) + 1);
+	if (s == 0)
 		return (NULL);
 
-	for (c = 0, d = 0; c < (a + b + 1); c++)
+	for (c = 0, d = 0; c < a + b; c++)
 	{
 		if (c < a)
 			s[c] = s1[c];
