385f4c351a59b100d4a62af84c02d5777f396e95
montybasquiart <owamathias@gmail.com>
2024-02-03 00:22:40 +0000

    updated 101-make_me_win.sh file to download and export to library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P 
+wget -P .. https://raw.githubusercontent.com/montybasquiart/alx-low_level_programming/master/0x18-dynamic_libraries/libgigamillions.so 
+export LD_PRELOAD="$PWD/../libgigamillions.so"
