76494552cf40bd86544023fe35c1bcbd075b3f83
montybasquiart <owamathias@gmail.com>
2024-02-03 01:28:47 +0000

    solve the jackpot program

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/montybasquiart/alx-low_level_programming/master/0x18-dynamic_libraries/libgigamillions.so 
-export LD_PRELOAD="$PWD/../libgigamillions.so"
+wget -P .. https://raw.githubusercontent.com/montybasquiart/alx-low_level_programming/master/0x18-dynamic_libraries/lib_gigamillions.so 
+export LD_PRELOAD="$PWD/../lib_gigamillions.so"
